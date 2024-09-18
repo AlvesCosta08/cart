a2137838dac6fbe9728eb021ec3a2503379d60ad
Alves08 <alvescosta74@gmail.com>
2024-09-18 12:51:44 -0300

    criado migrations

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,4 @@ FROM alpine:latest
 WORKDIR /root/
 COPY --from=builder /app/main .
 CMD ["./main"]
+
--- db/migrations/001_create_products_table.sql
+++ db/migrations/001_create_products_table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL
 );
