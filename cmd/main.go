35b57d9fec130ffd572bbfe1015b6d68d726833f
Alves08 <alvescosta74@gmail.com>
2024-09-22 13:21:36 -0300

    criado arquivo de config para variaveis de ambiente

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"cart-api/api"
+	"cart-api/config"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Carregar variáveis de ambiente
+	loadEnv()
+
+	// Carregar a configuração
+	cfg := config.LoadConfig()
+
+	// Iniciar o servidor da API
+	startServer(cfg)
+}
+
+// loadEnv carrega as variáveis do arquivo .env
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+}
+
+// startServer inicializa o servidor com a configuração fornecida
+func startServer(cfg config.Config) {
+	router := api.SetupRouter()
+
+	// Inicia o servidor e trata erros
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
+
+	log.Printf("Servidor rodando na porta :8080 com o banco: %s", cfg.DBName)
 }
